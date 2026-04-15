edc92d2b1067661b2e2baf6b9b1ce381bfda7956
usr40k <usr_40476@icloud.com>
2026-04-15 14:27:47 -0400

    Update injector.js

--- injector.js
+++ injector.js
@@ -30,7 +30,7 @@
         let origW = null;
 
         const cfg = {
-            dur: 600,
+            dur: 1200,
             chars: '.,·-─~+:;=*π""┐┌┘┴┬╗╔╝╚╬╠╣╩╦║░▒▓█▄▀▌▐■!?&#$@0123456789*',
             preserveSpaces: true,
             spread: 0.3,
